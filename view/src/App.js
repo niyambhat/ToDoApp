70c893e90f065217589dc56855ef54d34f3e096b
Niyam Bhat <niyambhat@192-168-1-101.tpgi.com.au>
2023-05-22 19:00:48 +0800

    Fetch requests

--- view/src/App.js
+++ view/src/App.js
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Constants from "./utilities/Constants";
 
-console.log(Constants)
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  function getPosts() {
+    const url = Constants.API_URL_GET_ALL_POSTS;
+    fetch(url, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(postsFromServer => {
+        setPosts(postsFromServer);
+        console.log(postsFromServer)
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
+
+  useEffect(()=>{
+  getPosts()
+  },[])
   return (
     <div className="App">
      
